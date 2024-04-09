10a55393b4ab7f0ddef511a17b71417c3f5dca67
Manuel Pickl <picklmanuel98@gmail.com>
2024-04-09 08:17:25 +0200

    added console info in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import User from "../User/User";
 import "./Header.scss"
+import wifi from "../../assets/icons/wifi.png"
+import battery from "../../assets/icons/battery.png"
 
 const Header = () => {
     const users = [
@@ -13,6 +15,17 @@ const Header = () => {
                 {users.map(user => {return(
                     <User user={user} key={user}/>
                 )})}
+            </div>
+            <div className="console-info">
+                <div className="time">
+                    {new Date().toTimeString().slice(0, 5)}
+                </div>
+                <div className="icon">
+                    <img src={wifi} alt="wifi" />
+                </div>
+                <div className="icon">
+                    <img src={battery} alt="battery" />
+                </div>
             </div>
         </div>
     );
