415c9d7bcd703e074e861cc40c3a85cfd6b6399c
Manuel Pickl <picklmanuel98@gmail.com>
2024-04-11 09:25:27 +0200

    reactive time in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,34 @@ import User from "../User/User";
 import "./Header.scss"
 import wifi from "../../assets/icons/wifi.png"
 import battery from "../../assets/icons/battery.png"
+import { useState, useEffect } from "react";
 
 const Header = () => {
     const users = [
         "Kass",
         "K.K."
-    ]
+    ];
+
+    const [currentTime, setCurrentTime] = useState(getTime());
+    const updateIntervallInMs = 1000;
+
+    function getTime(show24Hours: boolean = true): string {
+        return new Date().toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: !show24Hours,
+        });
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentTime(getTime());
+        }, updateIntervallInMs);
+
+        return (() => {
+            clearInterval(timer);
+        });
+    }, []);
 
     return (
         <div className="Header">
@@ -18,7 +40,7 @@ const Header = () => {
             </div>
             <div className="console-info">
                 <div className="time">
-                    {new Date().toTimeString().slice(0, 5)}
+                    {currentTime}
                 </div>
                 <div className="icon">
                     <img src={wifi} alt="wifi" />
