74870ea075c96801622663a0470c63aec649e69d
Manuel Pickl <picklmanuel98@gmail.com>
2024-04-12 12:11:49 +0200

    added comment for github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: process.env.NODE_ENV === 'production' ? '/Nintendo-Switch/' : '/'
+  // ToDo: look for automated solution
+  // only activate on github pages deploy
+  // base: process.env.NODE_ENV === 'production' ? '/nintendo-switch/' : '/'
 })
