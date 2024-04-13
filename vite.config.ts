772a397ffb65725716834e05bff890d67792be6b
Manuel Pickl <picklmanuel98@gmail.com>
2024-04-13 22:57:16 +0200

    removed unnecessary vite config code

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // ToDo: look for automated solution
-  // only activate on github pages deploy
-  // base: process.env.NODE_ENV === 'production' ? '/nintendo-switch/' : '/'
+  plugins: [react()]
 })
