180423bcd0ef2c2a63a7e706619469f5bfbdde1b
Manuel Pickl <picklmanuel98@gmail.com>
2024-04-13 23:18:37 +0200

    pwa update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Nintendo Switch',
+        short_name: 'Nintendo Switch',
+        description: 'Feel the Home Menu of the Nintendo Switch',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'landscape',
+        start_url: '.',
+        icons: [
+          {
+            src: '/logo.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
