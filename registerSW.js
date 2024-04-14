56713e1bba862b6211c5e36c12a3d1872f15d10a
Manuel Pickl <picklmanuel98@gmail.com>
2024-04-14 11:57:11 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/nintendo-switch/sw.js', { scope: '/nintendo-switch/' })})}
